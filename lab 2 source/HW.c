9c69c715b05aac39774e1a1bc005ee8009fdc334
Nikita Serikov <31020070+jerichofox@users.noreply.github.com>
2025-09-22 22:55:22 +0300

    Add files via upload

---
+++ lab 2 source/HW.c
@@ -0,0 +1,13 @@
+﻿#include <locale.h>;
+#include <stdio.h>;
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int A = 5, B = 10, C = 12, D = 20, R;
+	R = D - A - B - C;
+	printf("%+d", R);
+
+	return 0;
+}
---
+++ lab 2 source/Lab_2_3.c
@@ -0,0 +1,17 @@
+﻿#include <locale.h>;
+#include <stdio.h>;
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");		
+
+	printf("%d разделить на %d равно 00%3.7f\n\n", 2, 1323, 2. / 1323.);
+
+	printf("Дано:  %d, %d\nОтвет: 00%3.7f\n\n", 2, 1323, 2. / 1323.);  //1
+
+	printf("Дано:\n \t   %d\n \t%d\n \t____________\nОтвет:\n \t00%3.7f\n\n", 2, 1323, 2. / 1323.);  //2
+
+	printf("Дано:\n \t   %d\n \t%d\n \t____________\nОтвет:\n \t%+3.7f\n\n", 2, 1323, 2. / 1323.);  //3
+
+	return 0;
+}
